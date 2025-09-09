7bf4c61ab8e3459bba80be43acf74a4cc2622725
sdeithwen <sdeithwen@mail.ru>
2025-09-10 00:02:53 +0500

    ad

--- identidock/cmd.sh
+++ identidock/cmd.sh
@@ -6,7 +6,7 @@ if [ "$ENV" = 'DEV' ]; then
   exec python "identidock.py"
 elif [ "$ENV" = 'UNIT' ]; then
   echo "Running Unit Tests"
-  exec python "tests.py"
+#  exec python "tests.py"
 else
   echo "Running Production Server"
   exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
